be801b050f077fd0ed6e44d2a1571fc4ccca202e
vee-codes <17402407+vee-codes@users.noreply.github.com>
2025-08-15 14:42:13 -0700

    make game playable on terminal

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class ColorPalette:
         self.moves = moves
         self.target_color = target_color
         colors = ['r','b','g','y']
+        self.available_moves = self.moves
 
         if not grid:
             self.grid = [['' for c in range(num_cols)] for r in range(num_rows)]
@@ -23,6 +24,7 @@ class ColorPalette:
             self.grid = [row.copy() for row in grid]
 
     def fill(self,x:int,y:int,new_color:str)->bool:
+        """ Fills cell and adjacent cell with new_color"""
         # check bounds 
         if not (0<=x<self.num_cols and 0<=y<self.num_rows): 
             return False
@@ -52,5 +54,44 @@ class ColorPalette:
                     0<=ny<self.num_rows and
                     self.grid[nx][ny] == original_color):
                     queue.append((nx,ny))
-
+        self.available_moves -= 1
+        self.check_win()
         return True
+    
+    def check_win(self)->bool:
+        for r in self.grid:
+            for c in r:
+                if c != self.target_color:
+                    if self.available_moves == 0:
+                        print("You have lost, try again")
+                        input()
+                    else:
+                        self.display()
+        print("You have won!")
+        input()
+        return True
+
+    def display(self):
+        print(f"Target Color: {self.target_color}\n"
+              f"Moves Left: {self.available_moves}\n"
+              f"Pick a color and position as follows `<color> <row> <col>` to change color\n")
+        for row in self.grid:
+            print(' '+' '.join(cell for cell in row))
+        user_input = input()
+
+        args = user_input.replace(',',' ').split()
+        color = args[0]
+        x = int(args[1])
+        y = int(args[2]) 
+        print(f"Args: {color}, {x}, {y}")
+        self.fill(x,y,color)
+
+if __name__ == '__main__':
+
+    grid1 = [['g','g','g','g'],
+             ['g','b','b','g'],
+             ['g','b','b','g'],
+             ['g','g','g','g']]
+
+    game = ColorPalette(4,4,4,'r',grid1)
+    game.display()
